c3b9d2232f40bb5d38dc1c14330b06c078f22b2a
2-propanol <nuclear.fusion.247@gmail.com>
2020-10-10 16:25:11 +0900

    fix: ステージ移動と撮影タイミングがズレていた

--- acq_btf.py
+++ acq_btf.py
@@ -9,11 +9,11 @@ import numpy as np
 import pandas as pd
 from aries import Aries
 from nptyping import NDArray, Float16
-from tqdm import tqdm
+from tqdm import trange
 
 from transcoord import tlpltvpv_to_xyzu, xyzu_to_tlpltvpv
 
-NPZ_FILENAMETO_SAVE = "SweatNavyBlue-1D.btf.npz"
+NPZ_FILENAMETO_SAVE = "SweatNavyBlue-2D.btf.npz"
 
 # 露光時間（単位:us）
 ACQ_T_MIN_US = 25000
@@ -28,9 +28,6 @@ CROP_L = 800
 CROP_R = 1312
 CROP_T = 380
 CROP_B = 892
-
-cenetr = (CROP_L + CROP_R) / 2
-acq_img_size = (CROP_B - CROP_T, CROP_R - CROP_L)
 
 
 def acq_1d(start: float, stop: float, num: int) -> NDArray[(Any, 4), Float16]:
@@ -51,29 +48,32 @@ def acq_2d() -> NDArray[(Any, 4), Float16]:
     Returns:
         撮影したいtlpltvpvの組が入った`ndarray`: shape=(num, 4), dtype=np.float16
     """
-    # to_acq_tl = np.concatenate(
-    #     (
-    #         np.full(24, 15),
-    #         np.full(24, 30),
-    #         np.full(24, 45),
-    #         np.full(24, 60),
-    #         np.full(24, 75),
-    #     )
-    # )
-    # to_acq_pl = np.concatenate(
-    #     (
-    #         # np.arange(10, 89),
-    #         # np.arange(10, 89)
-    #         np.linspace(0, 345, int(360 / 15)),
-    #         np.linspace(0, 345, int(360 / 15)),
-    #         np.linspace(0, 345, int(360 / 15)),
-    #         np.linspace(0, 345, int(360 / 15)),
-    #         np.linspace(0, 345, int(360 / 15))
-    #         # np.linspace(10.0, 89.5, int(80 / 0.5)), np.linspace(10.0, 89.5, int(80 / 0.5))
-    #     )
-    # )
-
-    return np.array([(0, 0, 0, 0)], dtype=np.float16)
+    to_acq_tl = np.concatenate(
+        (
+            np.full(24, 15),
+            np.full(24, 30),
+            np.full(24, 45),
+            np.full(24, 60),
+            np.full(24, 75),
+        )
+    )
+    to_acq_pl = np.concatenate(
+        (
+            # np.arange(10, 89),
+            # np.arange(10, 89)
+            np.linspace(0, 345, int(360 / 15)),
+            np.linspace(0, 345, int(360 / 15)),
+            np.linspace(0, 345, int(360 / 15)),
+            np.linspace(0, 345, int(360 / 15)),
+            np.linspace(0, 345, int(360 / 15))
+            # np.linspace(10.0, 89.5, int(80 / 0.5)), np.linspace(10.0, 89.5, int(80 / 0.5))
+        )
+    )
+    num = len(to_acq_tl)
+    to_acq = np.zeros((num, 4), dtype=np.float16)
+    to_acq[:, 0] = to_acq_tl
+    to_acq[:, 1] = to_acq_pl
+    return to_acq
 
 
 def acq_4d() -> NDArray[(Any, 4), Float16]:
@@ -119,6 +119,8 @@ def schedule_acq(
 ) -> Tuple[NDArray[(Any, 4), Float16], NDArray[(Any, 4), Float16]]:
     """ステージ移動時間がなるべく短くなるように撮影順序を決める。
 
+    現在は"light", "pan", "tilt", "roll"の順にソートしているだけ
+
     Args:
         to_acq: 撮影したいtlpltvpvの組が入った`ndarray`, shape=(Any, 4)
 
@@ -130,7 +132,7 @@ def schedule_acq(
         columns=["tl", "pl", "tv", "pv"],
     )
     to_acq_xyzu = pd.DataFrame(
-        [tlpltvpv_to_xyzu(*tlpltvpv) for tlpltvpv in to_acq_tlpltvpv.values],
+        [tlpltvpv_to_xyzu(*tlpltvpv) for tlpltvpv in to_acq],
         columns=["pan", "tilt", "roll", "light"],
     )
     to_acq_xyzu = to_acq_xyzu.round(4)
@@ -146,7 +148,8 @@ def schedule_acq(
 def main() -> None:
     stage = Aries()
 
-    to_acq = acq_1d(-89, 89, 89 + 89 + 1)
+    # to_acq = acq_1d(-89, 89, 89 + 89 + 1)
+    to_acq = acq_2d()
     scheduled_tlpltvpv, scheduled_xyzu = schedule_acq(to_acq)
 
     stage.safety_stop = False
@@ -159,14 +162,18 @@ def main() -> None:
         return 1
 
     cap.set(cv2.CAP_PROP_GAIN, 0)
-    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     cap.average_num = 2
-    #TODO: CAP_FRAME > acq_img_size
+
+    # TODO: CAP_FRAME > acq_img_size
+    center = ((CROP_L + CROP_R) / 2, (CROP_T + CROP_B) / 2)
+    acq_img_size = (CROP_B - CROP_T, CROP_R - CROP_L)
 
     frames = np.empty((len(scheduled_xyzu), *acq_img_size, 3), dtype=np.float32)
-    tqdm_xyzu = tqdm(scheduled_xyzu)
-    for i, xyzu in enumerate(tqdm_xyzu):
+    tqdm_xyzu = trange(len(scheduled_xyzu))
+    for i in tqdm_xyzu:
+        xyzu = scheduled_xyzu[i]
         tqdm_xyzu.set_description(
             f"[X: {xyzu[0]:3.0f}, Y: {xyzu[1]:2.0f}, "
             + f"Z: {xyzu[2]:3.0f}, U: {xyzu[3]:3.0f}]"
@@ -174,10 +181,15 @@ def main() -> None:
 
         stage.sleep_until_stop()
         is_valid, frame = cap.readHDR(t_min=25000, t_max=250000, t_ref=100000)
-        stage.position = xyzu  # stageが動いている間にデモザイキングとクロップ処理をする
+
+        # stageが動いている間にデモザイキングとクロップ処理をする
+        if i + 1 < len(scheduled_xyzu):
+            stage.position = scheduled_xyzu[i + 1]
 
         frame = cd.demosaicing_CFA_Bayer_bilinear(frame, pattern="BGGR")
         # TODO: 射影変換
+        rot_mat = cv2.getRotationMatrix2D(center, xyzu[2], 1.0)
+        frame = cv2.warpAffine(frame, rot_mat, (width, height), flags=cv2.INTER_CUBIC)
         if is_valid:
             frames[i] = frame[CROP_T:CROP_B, CROP_L:CROP_R].astype(np.float32)
         else:
