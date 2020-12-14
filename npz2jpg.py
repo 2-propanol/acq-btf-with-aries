ca3bd51cacf0046ace382513093bbf78132a4299
2-propanol <nuclear.fusion.247@gmail.com>
2020-12-14 15:32:37 +0900

    add: tl, pl, tv, pvが含まれるファイル名で出力

--- npz2jpg.py
+++ npz2jpg.py
@@ -5,19 +5,28 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 
-BTF_FILENAME = "SweatNavyBlue-1D.btf.npz"
-JPEG_OUTPUT_DIRECTORY = "SweatNavyBlue-1D"
+BTF_FILENAME = "carbon_1D179.btf.npz"
+JPEG_OUTPUT_DIRECTORY = "carbon_1D179"
+
+EXTRACT_WITH_TLPLTVPV = False
+EXTRACT_WITH_SEQUENTIAL = True
 
 
 def main() -> None:
     npz = np.load(BTF_FILENAME)
     btf = npz["images"]
+    angles = npz["angles"]
 
     Path(JPEG_OUTPUT_DIRECTORY).mkdir(exist_ok=True)
 
-    for i, src in tqdm(enumerate(btf), total=len(btf)):
-        dst = np.clip(src*255,0,255)
-        cv2.imwrite(f"{JPEG_OUTPUT_DIRECTORY}/{i}.jpg", dst)
+
+    for i, (src, angle) in tqdm(enumerate(zip(btf, angles)), total=len(btf)):
+        dst = np.clip(src * 255, 0, 255)
+        if EXTRACT_WITH_SEQUENTIAL:
+            cv2.imwrite(f"{JPEG_OUTPUT_DIRECTORY}/{i}.png", dst)
+        if EXTRACT_WITH_TLPLTVPV:
+            cv2.imwrite(f"{JPEG_OUTPUT_DIRECTORY}/tl{angle[0]:.0f}_pl{angle[1]:.0f}_tv{angle[2]:.0f}_pv{angle[3]:.0f}.png", dst)
+
 
 if __name__ == "__main__":
     main()
